6631b2842af2c7aea15396c8b04c3d3fe872a221
lasertsole <56973455+lasertsole@users.noreply.github.com>
2023-03-25 21:28:59 +0800

    沉浮兔1.0.0

---
+++ animate_forum_backend/index.js
@@ -0,0 +1,87 @@
+const express = require("express")
+const cors = require('cors')
+const {fasterLogin, login, register, submitUserProfile, changeUserProfile, submitNewName} = require("./services/account.js")
+
+const {getnewWorkBox, submitnewWorkBoxImage, submitnewWorkBox, getLikeStatusArr, changeLike, searchNewWorkBox} = require("./services/newWork.js")
+const { getTrendsBox, submitTrendsBoxImage, submitTrendsBox, searchTrendsBox} = require("./services/trends.js")
+
+const {getShowCaseBox, submitShowCaseBoxImage, submitShowCaseBox, searchShowCaseBox, searchShowCaseBoxBySearchID} = require("./services/showcase.js")
+const {getRequireBox, submitRequireBoxImage, submitRequireBox, searchRequireBox, searchRequireBoxBySearchID} = require("./services/require.js")
+
+const {getSessionBox, getPersonList, submitChat} = require("./services/session.js")
+const {getAnnouncementBox} = require("./services/announcement.js")
+
+const path = require('path')
+
+const PORT = 8000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/files',express.static(path.join(__dirname,"../../files")));
+
+// var multipart = require('connect-multiparty');
+// var multipartMiddleware = multipart();
+// app.use(multipart({uploadDir:path.join(__dirname,"../../files")}));
+
+// app.post('/upload',multipartMiddleware, function (req,res) {
+// 	console.log(req.body,req.files);//分别返回body，文件属性，以及文件存放地址
+// })
+
+/*测试连接性*/
+app.get('/',(req,res)=>{
+	res.send("Helloworld");
+})
+
+/*注册*/
+app.post('/register', register);
+
+/*登录*/
+app.post('/login', login)//普通登录
+app.get('/fasterLogin', fasterLogin)//token登录
+
+/*更改用户个人信息*/
+app.post('/submitUserProfile', submitUserProfile);//上传头像
+app.post('/changeUserProfile', changeUserProfile);//更改头像
+app.post('/submitNewName', submitNewName);//更改用户名字
+
+/*主页-新作品*/
+app.get("/getnewWorkBox", getnewWorkBox)//获取新作品盒子方法
+app.post("/submitnewWorkBoxImage", submitnewWorkBoxImage)//用户上传新作品盒子的图片
+app.post("/submitnewWorkBox", submitnewWorkBox)//用户上传新作品盒子
+app.get("/getLikeStatusArr", getLikeStatusArr)//改变点赞列表
+app.post("/changeLike", changeLike)//改变点赞状态
+app.get("/searchNewWorkBox", searchNewWorkBox)//搜索新作品盒子
+
+/*主页-动态*/
+app.get("/getTrendsBox", getTrendsBox)//获取关注盒子方法
+app.post("/submitTrendsBoxImage", submitTrendsBoxImage)//用户上传关注盒子的图片
+app.post("/submitTrendsBox", submitTrendsBox)//用户上传动态盒子
+app.get("/searchTrendsBox", searchTrendsBox);//搜索动态盒子
+
+/*约稿-橱窗*/
+app.get("/getShowCaseBox", getShowCaseBox)//获取橱窗盒子方法
+app.post("/submitShowCaseBoxImage", submitShowCaseBoxImage)//用户上传橱窗盒子的图片
+app.post("/submitShowCaseBox", submitShowCaseBox)//用户上传推荐盒子
+app.get("/searchShowCaseBox", searchShowCaseBox)//搜索橱窗盒子
+app.get("/searchShowCaseBoxBySearchID", searchShowCaseBoxBySearchID)//根据searchID查找橱窗盒子信息
+
+/*约稿-需求帖*/
+app.get("/getRequireBox", getRequireBox)//获取需求盒子方法
+app.post("/submitRequireBoxImage", submitRequireBoxImage)//用户上传需求盒子的图片
+app.post("/submitRequireBox", submitRequireBox)//用户上传需求盒子
+app.get("/searchRequireBox", searchRequireBox)//搜索需求盒子
+app.get("/searchRequireBoxBySearchID", searchRequireBoxBySearchID)//根据searchID查找需求盒子信息
+
+/*消息-会话*/
+app.get("/getSessionBox", getSessionBox)//获取消息盒子方法
+app.post("/getPersonList", getPersonList)//获取右侧联系人列表
+app.post("/submitChat", submitChat)//提交聊天内容
+
+/*消息-公告*/
+app.get("/getAnnouncementBox", getAnnouncementBox)//获取公告盒子方法
+
+/*启动后台，监听端口为PORT*/
+app.listen(PORT,()=>{
+	console.log(`HTTP服务在${PORT}端口启动...`);
+});
---
+++ animate_forum_backend/services/require.js
@@ -0,0 +1,86 @@
+const db = require('../db.js')
+const { fileUpload } = require('./manner.js')
+
+/***********************约稿-需求帖***********************/
+async function getRequireBox(req, res) {//获取需求盒子方法
+    let sql = `select title,describe_require,describe_image,money,tag,calendar,requireBox.id,
+    profile,username,search_id from requireBox left join user_login on requireBox.id=user_login.id
+     order by requireBox.created_time desc limit 50`
+    let result = await db.query(sql)
+    if (result.error) {
+        res.send({error: '获取需求盒子失败'})
+    } else {
+        res.send(result)
+    }
+};
+
+async function submitRequireBoxImage(req, res) {//用户上传需求盒子
+    let result = await fileUpload(req);
+    if(!result.error){
+        res.send(result);
+    }else{
+        res.send({error:"作品上传失败"});
+    }
+    console.log(result);
+};
+
+async function submitRequireBox(req, res) {//用户上传需求盒子
+    let data = req.body;
+
+    let title = data.title;
+    let describe = data.describe;
+    let describe_image = data.describe_image;
+    if(describe_image==undefined){
+        describe_image="";
+    }
+    else{
+        describe_image = "/files/" + describe_image;
+    }
+    let money = data.money;
+    let tag = data.tag;
+    let calendar= data.calendar;
+    let id = data.id;
+    let username = data.username;
+
+    let sql = 'insert into requirebox (title, describe_require, describe_image, money, tag, calendar, id) values (?, ?, ?, ?, ?, ?, ?)'
+    let result = await db.query(sql, [title, describe, describe_image, money, tag, calendar, id]);
+    if(result.error||result.length==0){
+        res.send({error:"数据库插入失败"});
+    }else{
+        res.send({success:"数据库插入成功"});
+    }
+};
+
+async function searchRequireBox(req, res){//搜索需求盒子
+    let data=req.query;
+    let searchContent = data.searchContent;
+
+    searchContent = "%"+searchContent+"%";
+    let sql = `select title,describe_require,describe_image,money,tag,calendar,requireBox.id,profile,
+    username from requireBox left join user_login on requireBox.id=user_login.id where title like ? or
+     username like ? or describe_require like ? order by requireBox.created_time desc limit 50`;
+    let result = await db.query(sql, [searchContent, searchContent, searchContent]);
+    
+    if(!result.error){res.send(result);}else{res.send({error:"查找发送错误"});};
+}
+
+async function searchRequireBoxBySearchID(req, res){//根据searchID查找需求盒子信息
+    let data=req.query;
+    let search_id=data.search_id;
+
+    let sql = `select title,describe_require,describe_image,money,tag,calendar,requireBox.id,profile,
+    username from requireBox left join user_login on requireBox.id=user_login.id where search_id=?`;
+
+    let result = await db.query(sql, [search_id]);
+    
+    if(!result.error){res.send(result[0]);}else{res.send({error:"查找发送错误"});};
+}
+
+module.exports={
+    //约稿-需求帖
+    getRequireBox,
+    submitRequireBoxImage,
+    submitRequireBox,
+    searchRequireBox,
+    searchRequireBoxBySearchID,
+}
